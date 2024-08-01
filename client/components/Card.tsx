d001989bec30f7eecb8c18b67f08fd7a70033c9d
edward-soung-24 <170850092+EddieWeddie11@users.noreply.github.com>
2024-08-02 11:31:50 +1200

    Finished Create-Card-Component

---
+++ client/components/Card.tsx
@@ -0,0 +1,16 @@
+function Card () {
+  return(
+    <div>
+      
+    </div>
+  )
+}
+
+default export Card
+
+
+// linking to the data base i.e colour
+// what the card looks like
+// needs an onClick event 
+// ifFlipped !ifFlipped
+// ifMatched !ifMatched
