4d05a8efe4cc0071052fb43a7ff8fa8b670ecc2f
edward-soung-24 <170850092+EddieWeddie11@users.noreply.github.com>
2024-08-02 11:36:25 +1200

    Updated Card Component

--- client/components/Card.tsx
+++ client/components/Card.tsx
@@ -1,16 +1,11 @@
-function Card () {
-  return(
-    <div>
-      
-    </div>
-  )
+function Card() {
+  return <div></div>
 }
 
-default export Card
-
+export default Card
 
 // linking to the data base i.e colour
 // what the card looks like
-// needs an onClick event 
+// needs an onClick event
 // ifFlipped !ifFlipped
 // ifMatched !ifMatched
